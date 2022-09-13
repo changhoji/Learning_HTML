de15fe56b34178bc01fd187fd50d9ccf9c737010
2021035487 <onkkno1@gmail.com>
2022-09-13 14:28:07 +0900

    Studied create topic

--- JavaScript/생활코딩/react-app/src/App.js
+++ JavaScript/생활코딩/react-app/src/App.js
@@ -17,7 +17,7 @@ function Nav(props) {
   for (let i = 0; i < props.topics.length; i++) {
     let t = props.topics[i];
     lis.push(<li key={t.id}>
-      <a id={t.id} href={'/read/'+t.id} onClick={event=> {
+      <a id={t.id} href={'/read/'+t.id} onClick={(event)=> {
         event.preventDefault();
         props.onChangeMode(Number(event.target.id));
       }}>{t.title}</a>
@@ -39,18 +39,34 @@ function Article(props) {
     </article>
   )
 }
+function Create(props) {
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=> {
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title" /></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
 
 function App() {
   // const _mode = useState("WELCOME");
   // const mode = _mode[0];
   // const setMode = _mode[10];
   const [mode, setMode] = useState("WELCOME");
-  const [id, steId] = useState(null);
-  const topics = [
+  const [id, setId] = useState(null);
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id: 1, title:'html', body:'html is ...'},
     {id: 2, title: 'css', body: "css is ..."},
     {id: 3, title: 'javascript', body: "javascript is ..."}
-  ];
+  ]);
   let content = null;
   if (mode === "WELCOME") {
     content = <Article title="Welcome" body="Hello, Web"></Article>;
@@ -65,6 +81,17 @@ function App() {
     }
     content = <Article title={title} body={body}></Article>
   }
+  else if (mode === "CREATE") {
+    content = <Create onCreate={(_title, _body)=> {
+      const newTopic = {id: nextId, title: _title, body: _body};
+      const newTopics = [...topics];
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode("READ");
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
+  }
   return (
     <div>
       <Header title="REACT" onChangeMode={()=> {
@@ -72,9 +99,13 @@ function App() {
       }}></Header>
       <Nav topics={topics} onChangeMode={(id)=> {
         setMode("READ");
-        steId(id);
+        setId(id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode("CREATE");
+      }}>Create</a>
     </div>
   );
 }
